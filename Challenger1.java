aa902867b7557cbd933aa3a8bc2f003b88850b00
edsonbnasc <126781804+edsonbnasc@users.noreply.github.com>
2023-03-26 21:42:44 -0300

    desafio Challenger em uma unica linha

---
+++ Challenger1.java
@@ -0,0 +1,7 @@
+public class Challenger1{
+    public static void main(String[] args){
+        int myName = 20;
+        int magicNumber = ((((((myName*myName)+myName)/myName)+17)-myName)/6);
+        System.out.println(magicNumber);
+    }
+}
